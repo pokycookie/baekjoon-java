355c75e1ac36c39cf6eb35555d0d4564e6ca4023
pokycookie <0cookieboy0@gmail.com>
2023-11-22 20:10:42 +0900

    init: initialize git

---
+++ src/baekjoon/backtracking/Solution.java
@@ -0,0 +1,13 @@
+package baekjoon.backtracking;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+    }
+}
