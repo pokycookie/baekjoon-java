b7e90285c2eb7e0872e4fe58457b7a09122bb612
pokycookie <0cookieboy0@gmail.com>
2024-02-11 16:47:01 +0900

    feat: 19597.java

--- src/baekjoon/Solution19597.java
+++ src/baekjoon/Solution19597.java
@@ -3,12 +3,15 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Solution19597 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     static int n;
     static String[] seq;
+    static Stack<String> stack;
+    static String ans;
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
@@ -20,40 +23,37 @@ public class Solution19597 {
             for (int i = 0; i < n; i++) {
                 seq[i] = br.readLine();
             }
-            sb.append(solution()).append("\n");
+            ans = null;
+            stack = new Stack<>();
+            stack.add("0");
+            backtracking(1, seq[0]);
+            stack.pop();
+            stack.add("1");
+            backtracking(1, reverse(seq[0]));
+            sb.append(ans).append("\n");
         }
         System.out.println(sb);
     }
 
-    static String solution() {
-        String c = compare(false);
-        if (c != null) {
-            return c;
+    static void backtracking(int idx, String prev) {
+        if (ans != null) {
+            return;
         }
-        return compare(true);
-    }
-
-    static String compare(boolean reverse) {
-        String[] strs = new String[n];
-        System.arraycopy(seq, 0, strs, 0, n);
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(reverse ? 1 : 0);
-        if (reverse) {
-            strs[0] = reverse(strs[0]);
+        if (idx == n) {
+            ans = String.join("", stack);
+            return;
         }
-        for (int i = 1; i < n; i++) {
-            if (strs[i].compareTo(strs[i - 1]) >= 0) {
-                sb.append(0);
-                continue;
-            }
-            sb.append(1);
-            strs[i] = reverse(strs[i]);
-            if (strs[i].compareTo(strs[i - 1]) < 0) {
-                return null;
-            }
+        if (seq[idx].compareTo(prev) >= 0) {
+            stack.add("0");
+            backtracking(idx + 1, seq[idx]);
+            stack.pop();
         }
-        return sb.toString();
+        String rev = reverse(seq[idx]);
+        if (rev.compareTo(prev) >= 0) {
+            stack.add("1");
+            backtracking(idx + 1, rev);
+            stack.pop();
+        }
     }
 
     static String reverse(String s) {
