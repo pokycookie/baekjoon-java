d1a0ae50bf1d068543c8caec6aebea0201d34c7d
pokycookie <0cookieboy0@gmail.com>
2024-03-22 23:00:25 +0900

    feat: 13950.java

---
+++ src/baekjoon/Solution13950.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution13950 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        
+    }
+}
