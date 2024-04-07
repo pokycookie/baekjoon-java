a5019f3c259d675c11c3b82f865c8752c8e0861c
pokycookie <0cookieboy0@gmail.com>
2024-04-07 12:56:52 +0900

    feat: 1708.java

--- src/baekjoon/geometry/Solution1708.java
+++ src/baekjoon/geometry/Solution1708.java
@@ -3,8 +3,9 @@ package baekjoon.geometry;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Stack;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Solution1708 {
@@ -14,53 +15,51 @@ public class Solution1708 {
     static int N;
     static Pos pivot;
     static Pos[] points;
-    static Stack<Pos> stack;
+    static Deque<Pos> stack;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         points = new Pos[N];
 
         st = new StringTokenizer(br.readLine());
-        pivot = new Pos(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
-        points[0] = pivot;
+        points[0] = new Pos(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+        pivot = points[0];
 
         for (int i = 1; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             points[i] = new Pos(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
-            if (pivot.y > points[i].y) pivot = points[i];
-            else if (pivot.y == points[i].y && pivot.x > points[i].x) pivot = points[i];
+            if (points[i].y < pivot.y) pivot = points[i];
+            else if (points[i].y == pivot.y && points[i].x < pivot.x) pivot = points[i];
         }
 
         Arrays.sort(points, (p1, p2) -> {
-            long deg = ccw(pivot, p1, p2);
-            long dst = distance(pivot, p1) - distance(pivot, p2);
+            long deg = ccw(pivot.to(p2), p2.to(p1));
+            long dst = pivot.dist(p1) - pivot.dist(p2);
             if (deg == 0) return dst == 0 ? 0 : dst > 0 ? 1 : -1;
-            return deg > 0 ? -1 : 1;
+            return deg > 0 ? 1 : -1;
         });
 
-        stack = new Stack<>();
-        stack.add(pivot);
+        stack = new ArrayDeque<>();
+        stack.push(points[0]);
+        stack.push(points[1]);
 
-        for (int i = 1; i < N; i++) {
-            Pos next = points[i];
-            while (stack.size() > 1) {
-                Pos first = stack.get(stack.size() - 2);
-                Pos second = stack.get(stack.size() - 1);
-                long ret = ccw(first, second, next);
-                if (ret > 0) break;
-                stack.pop();
-            }
-            stack.add(points[i]);
+        int i = 2;
+        while (i < N) {
+            Pos curr = points[i];
+            Pos prev1 = stack.pop();
+            Pos prev2 = stack.peek();
+
+            long res = ccw(prev2.to(prev1), prev1.to(curr));
+            if (res < 0) continue;
+            if (res > 0) stack.push(prev1);
+            stack.push(curr);
+            i++;
         }
         System.out.println(stack.size());
     }
 
-    static long ccw(Pos p1, Pos p2, Pos p3) {
-        return (p1.x * p2.y + p2.x * p3.y + p3.x * p1.y) - (p2.x * p1.y + p3.x * p2.y + p1.x * p3.y);
-    }
-
-    static long distance(Pos p1, Pos p2) {
-        return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+    static long ccw(Pos v1, Pos v2) {
+        return v1.x * v2.y - v1.y * v2.x;
     }
 
     static class Pos {
@@ -70,6 +69,14 @@ public class Solution1708 {
         public Pos(long x, long y) {
             this.x = x;
             this.y = y;
+        }
+
+        long dist(Pos o) {
+            return (x - o.x) * (x - o.x) + (y - o.y) * (y - o.y);
+        }
+
+        Pos to(Pos o) {
+            return new Pos(o.x - x, o.y - y);
         }
     }
 }
